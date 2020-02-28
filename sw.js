03d8b0bb366d53b721420d44f7cab254aa834136
Rene <renedahlhansen@gmail.com>
2020-02-28 10:56:22 +0100

    adding a offline page, and a change the fetch

--- sw.js
+++ sw.js
@@ -6,17 +6,48 @@ self.addEventListener('install', function (e) {
                 '/Shoplist/',
                 '/Shoplist/index.html',
                 '/Shoplist/style.css',
-                '/Shoplist/script.js'
+                '/Shoplist/script.js',
+                '/Shoplist/offline.html'
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', function (e) {
-    console.log(e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
+// self.addEventListener('fetch', function (e) 
+// {
+//     console.log(e.request.url);
+//     e.respondWith(
+//         caches.match(e.request).then(function (response) {
+//             return response || fetch(e.request);
+//         })
+//     );
+// });
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache');
+                    return response;
+                }
+                console.log('Network request for ', event.request.url);
+                return fetch(event.request).then(response => {
+                    // TODO 4 - Add fetched files to the cache, we first cache the pages then the user viset the pages, if the user is offline
+                    return caches.open(staticCacheName).then(function (cache) {
+                        cache.put(event.request.url, response.clone())
+                        return response;
+                    })
+                })
+
+
+            }).catch(error => {
+
+                // TODO 6 - Respond with custom offline page
+                console.log('Error, ', error);
+                return caches.match('/Shoplist/offline.html');
+
+            })
     );
 });
