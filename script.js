fdf0ee3f2754473de308e99e4ffc367e5f43875a
Rene Dahl <rene_dahl@hotmail.com>
2020-03-26 12:27:47 +0100

    Adding the shop-store to the project, testing it

--- script.js
+++ script.js
@@ -19,39 +19,41 @@
 //        .then(function () { console.log('Service Worker Registered'); });
 //}
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/Shoplist/sw.js')
-        .then(function () { console.log('Service Worker Registered'); });
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/Shoplist/sw.js").then(function() {
+    console.log("Service Worker Registered");
+
+    // .register('/Shoplist/sw.js')
+  });
 }
 
 // Code to handle install prompt on desktop
 
 let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+const addBtn = document.querySelector(".add-button");
+addBtn.style.display = "none";
 
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
+window.addEventListener("beforeinstallprompt", e => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = "block";
 
-    addBtn.addEventListener('click', (e) => {
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the shopping list prompt');
-            } else {
-                console.log('User dismissed the shopping list prompt');
-            }
-            deferredPrompt = null;
-        });
+  addBtn.addEventListener("click", e => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = "none";
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the shopping list prompt");
+      } else {
+        console.log("User dismissed the shopping list prompt");
+      }
+      deferredPrompt = null;
     });
+  });
 });
